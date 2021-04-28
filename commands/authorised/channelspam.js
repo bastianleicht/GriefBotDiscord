d38bed1ab6b6e9d4dfd464a277c6559200856fad
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-04-28 16:54:31 +0200

    Added: channelspam Command
    Dieser Command erstellt eine anzahl an Channeln welche aus Unicode Charakteren bestehen.

---
+++ commands/authorised/channelspam.js
@@ -0,0 +1,36 @@
+/*
+ *   Copyright (c) 2021 Bastian Leicht
+ *   All rights reserved.
+ *   https://bastianleicht.de/license
+ */
+// jshint esversion: 8
+
+module.exports.run = async (client, message, args) => {
+    message.delete().catch(()=> { });
+
+    let min = 20;   //Minimale länge des Strings
+    let max = 200;  //Maximale länge des Strings
+
+    function generateString() {
+        let result = ' ';
+        const charactersLength = client.unicodeText.length;
+        for ( let i = 0; i < Math.floor(Math.random() * (max - min + 1)) + min; i++ ) {
+            result += client.unicodeText.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return result;
+    }
+
+    let i = 0;
+
+    while (i < args[0]) {
+        try {
+            await message.guild.channels.create(generateString());
+        } catch(e) {
+            console.log(e.stack);
+        }
+        i++;
+
+    }
+
+};
