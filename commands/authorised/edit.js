e6b2f5f6b38708f9605d2f6d29f1b1872e214af3
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-04-28 21:57:48 +0200

    Update: Ganzen Text nutzen

--- commands/authorised/edit.js
+++ commands/authorised/edit.js
@@ -9,24 +9,24 @@ module.exports.run = async (client, message, args) => {
     message.delete().catch(()=> { });
 
     if(args[0] === 'all' && args[1] === 'channels') {
-        let name = message.content.split(" ")[3];
+        let name = message.content.split(' ').slice(3).join(' ');
         let channels = message.guild.channels.cache;
         channels.forEach(channel => {
             channel.setName(name).catch(() => { });
         });
     } else if(args[0] === 'all' && args[1] === 'roles') {
-        let name = message.content.split(" ")[3];
+        let name = message.content.split(' ').slice(3).join(' ');
         let roles = message.guild.roles.cache;
         roles.forEach(role => {
             role.setName(name).catch(() => { });
         });
     } else if(args[0] === 'channel') {
         let channel = message.content.split(" ")[2];
-        let name = message.content.split(" ")[3];
+        let name = message.content.split(' ').slice(3).join(' ');
         message.guild.channels.cache.find(x => x.name === channel).setName(name).catch(() => { });
     } else if(args[0] === 'role') {
         let role = message.content.split(" ")[2];
-        let name = message.content.split(" ")[3];
+        let name = message.content.split(' ').slice(3).join(' ');
         message.guild.roles.cache.find(x => x.name === role).setName(name).catch(() => { });
     }
 
